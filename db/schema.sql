7db3acb3e4b5da69217d8823fdd854eebe9805fa
Aleck Tuluenga <aleckbryson@gmail.com>
2020-09-10 16:26:04 -0400

    MVC folders added with required js files need to route connecting; handlebar files updated as well

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+### Schema
+
+DROP DATABASE IF EXISTS burger_DB;
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	hungry BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burger_db;
+
+INSERT INTO burgers (name) VALUES ('Big Mick');
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name, hungry) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (name, hungry) VALUES ('Chicken Sandwich', true);
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
